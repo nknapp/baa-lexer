a6ec1a9b5203078e5ba50131613306e0d381dc2c
Nils Knappmeier <npm@knappi.org>
2023-04-05 23:29:43 +0200

    combineRegex

--- src/utils/combineRegex.test.ts
+++ src/utils/combineRegex.test.ts
@@ -1,62 +1,84 @@
-import {describe, expect, it} from "vitest";
-import {combineRegex} from "./combineRegex";
-
+import { describe, expect, it } from "vitest";
+import { combineRegex } from "./combineRegex";
 
 describe("combineRegex", () => {
-    it("returns an object with lastIndex, lastGroup, lastMatch", () => {
-        expect(combineRegex([/a/])).toHaveProperty("lastIndex")
-        expect(combineRegex([/a/])).toHaveProperty("lastGroup")
-        expect(combineRegex([/a/])).toHaveProperty("lastMatch")
-    })
+  it("returns an object with lastIndex, lastRegex, lastMatch", () => {
+    expect(combineRegex([/a/])).toHaveProperty("lastIndex");
+    expect(combineRegex([/a/])).toHaveProperty("lastRegex");
+    expect(combineRegex([/a/])).toHaveProperty("lastMatch");
+  });
 
-    it("initially, lastIndex is 0, lastMatch is null, lastGroup is -1", () => {
-        expect(combineRegex([/a/]).lastIndex).toBe(0)
-        expect(combineRegex([/a/]).lastMatch).toBe(null)
-        expect(combineRegex([/a/]).lastGroup).toBe(-1)
-    })
+  it("initially, lastIndex is 0, lastMatch is null, lastRegex is -1", () => {
+    expect(combineRegex([/a/]).lastIndex).toBe(0);
+    expect(combineRegex([/a/]).lastMatch).toBe(null);
+    expect(combineRegex([/a/]).lastRegex).toBe(-1);
+  });
 
-    describe("when initialized with a single regex, calling the exec method", () => {
+  describe("when initialized with a single regex, calling the exec method", () => {
+    it("returns true if the regex matches", () => {
+      const matchingA = combineRegex([/a/]);
+      expect(matchingA.exec("a")).toBe(true);
+    });
+    it("returns false if the regex does not match", () => {
+      const matchingA = combineRegex([/a/]);
+      expect(matchingA.exec("b")).toBe(false);
+    });
 
+    it("stores the match in lastMatch", () => {
+      const matchingA = combineRegex([/a/]);
+      matchingA.exec("a");
+      expect(matchingA.lastMatch).toBe("a");
+    });
 
-        it("returns true if the regex matches", () => {
-            const matchingA = combineRegex([/a/]);
-            expect(matchingA.exec("a")).toBe(true)
-        })
-        it("returns false if the regex does not match", () => {
-            const matchingA = combineRegex([/a/]);
-            expect(matchingA.exec("b")).toBe(false)
-        })
+    it("stores 0 in lastRegex", () => {
+      const matchingA = combineRegex([/a/]);
+      matchingA.exec("a");
+      expect(matchingA.lastRegex).toBe(0);
+    });
 
-        it("stores the match in lastMatch", () => {
-            const matchingA = combineRegex([/a/]);
-            matchingA.exec("a");
-            expect(matchingA.lastMatch).toBe("a")
-        })
+    it("stores lastIndex", () => {
+      const matchingA = combineRegex([/a/]);
+      matchingA.exec("a");
+      expect(matchingA.lastIndex).toBe(1);
+    });
 
-        it("stores 0 in lastGroup", () => {
-            const matchingA = combineRegex([/a/]);
-            matchingA.exec("a");
-            expect(matchingA.lastGroup).toBe(0)
-        })
+    it("advances in multiple matches", () => {
+      const matchingA = combineRegex([/a/]);
+      matchingA.exec("aba");
+      matchingA.exec("aba");
+      expect(matchingA.lastIndex).toBe(3);
+      expect(matchingA.lastMatch).toBe("a");
+      expect(matchingA.lastRegex).toBe(0);
+    });
 
-        it("stores lastIndex", () => {
-            const matchingA = combineRegex([/a/]);
-            matchingA.exec("a");
-            expect(matchingA.lastIndex).toBe(1)
-        })
+    it("with sticky option set, does not jump chars", () => {
+      const matchingA = combineRegex([/a/], { sticky: true });
+      expect(matchingA.exec("ba")).toBe(false);
+    });
+  });
 
-        it("advances in multiple matches", () => {
-            const matchingA = combineRegex([/a/]);
-            matchingA.exec("aba");
-            matchingA.exec("aba");
-            expect(matchingA.lastIndex).toBe(3)
-            expect(matchingA.lastMatch).toBe("a")
-            expect(matchingA.lastGroup).toBe(0)
-        })
+  describe("with multiple regexes", () => {
+      it("also matches the second string string", () => {
+          const matchingAB = combineRegex([/a/,/b/])
+          expect(matchingAB.exec("xb")).toBe(true)
+      })
 
-        it("with sticky option set, does not jump chars", () => {
-            const matchingA = combineRegex([/a/], {sticky: true});
-            expect(matchingA.exec("ba")).toBe(false)
-        })
-    })
-})
+      it("stores the match for the seconds regex", () => {
+          const matchingAB = combineRegex([/a/,/b/])
+          matchingAB.exec("xb")
+          expect(matchingAB.lastMatch).toBe("b")
+      })
+
+      it("stores lastRegex of 1 for the second regex", () => {
+          const matchingAB = combineRegex([/a/,/b/])
+          matchingAB.exec("xb")
+          expect(matchingAB.lastRegex).toBe(1)
+      })
+
+      it("stores lastIndex for the second regex", () => {
+          const matchingAB = combineRegex([/a/,/b/])
+          matchingAB.exec("xb")
+          expect(matchingAB.lastIndex).toBe(2)
+      })
+  })
+});
--- src/utils/combineRegex.ts
+++ src/utils/combineRegex.ts
@@ -1,33 +1,40 @@
 interface CombinedRegex {
   lastMatch: string | null;
   lastIndex: number;
-  lastGroup: number;
+  lastRegex: number;
 
   exec(string: string): boolean;
 }
 
-export function combineRegex(regExps: RegExp[], { sticky = false } = {}): CombinedRegex {
-  return new CombinedRegexImpl(regExps, {sticky});
+export function combineRegex(
+  regExps: RegExp[],
+  { sticky = false } = {}
+): CombinedRegex {
+  return new CombinedRegexImpl(regExps, { sticky });
 }
 
 class CombinedRegexImpl implements CombinedRegex {
   lastMatch: string | null = null;
   lastIndex = 0;
-  lastGroup = -1;
-  regex: RegExp
+  lastRegex = -1;
+  regex: RegExp;
 
-  constructor(regexes: RegExp[],  { sticky = false } = {}) {
-      this.regex = new RegExp(regexes[0].source,sticky ? "y" : "g")
+  constructor(regexes: RegExp[], { sticky = false } = {}) {
+    const sources = regexes.map((regex) => "(" + regex.source + ")");
+    this.regex = new RegExp(sources.join("|"), sticky ? "y" : "g");
   }
 
   exec(string: string): boolean {
-      const match = this.regex.exec(string);
-      if (match != null) {
-          this.lastMatch = match[0]
-          this.lastGroup = 0
-          this.lastIndex = this.regex.lastIndex
-          return true
+    const match = this.regex.exec(string);
+    if (match != null) {
+      this.lastMatch = match[0];
+      this.lastRegex = 0;
+      while (match[this.lastRegex + 1] == null) {
+        this.lastRegex++;
       }
-      return false;
+      this.lastIndex = this.regex.lastIndex;
+      return true;
+    }
+    return false;
   }
 }
