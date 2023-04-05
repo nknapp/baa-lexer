5acdb8569c856a4c7ba6cf9f66c89c6d4d7515d6
Nils Knappmeier <npm@knappi.org>
2023-04-05 23:17:10 +0200

    sticky

--- src/BaaLexer.ts
+++ src/BaaLexer.ts
@@ -1,8 +1,11 @@
 import {Lexer, LexerTypings, MooStates, Token} from "./types";
 
 export class BaaLexer<T extends LexerTypings> implements Lexer<T> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
   constructor(states: MooStates<T>) {
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
   *lex(string: string): IterableIterator<Token<T>> {
   }
 }
